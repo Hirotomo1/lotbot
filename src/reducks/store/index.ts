cd72ff172589169ef2a8ec905fca18be84e9bb0f
Hirotomo <d.sasaki0120@gmail.com>
2021-06-18 13:07:52 +0900

    修正

--- src/reducks/store/index.ts
+++ src/reducks/store/index.ts
@@ -2,3 +2,5 @@ export { changeMargin } from "../margins/actions";
 export { changePips } from "../pipses/actions";
 export { changePercentage } from "../percentages/actions";
 export { doAnswer } from "../answers/actions";
+export { changeLat } from "../lats/actions";
+export { changeLng } from "../lngs/actions";
--- src/reducks/store/store.ts
+++ src/reducks/store/store.ts
@@ -4,12 +4,16 @@ import { MarginsReducer } from "../margins/reducers";
 import { PercentagesReducer } from "../percentages/reducers";
 import { PipsesReducer } from "../pipses/reducers";
 import { AnswersReducer } from "../answers/reducers";
+import { LatsReducer } from "../lats/reducers";
+import { LngsReducer } from "../lngs/reducers";
 
 const rootReducer = combineReducers({
   margins: MarginsReducer,
   percentages: PercentagesReducer,
   pipses: PipsesReducer,
   answers: AnswersReducer,
+  lngs: LngsReducer,
+  lats: LatsReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
